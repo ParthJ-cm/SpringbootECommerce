bb8d2a69efd8c5a1ebb9c03f88c1eb7b071783e6
ParthJ <parth.joshi@tatvasoft.com>
2025-10-09 11:18:22 +0530

    Added Cart Services Rest API Calls. and Added Cart & Product Front Pages.

---
+++ cart-service/src/main/java/com/shop/cart_service/dto/CartMapper.java
@@ -0,0 +1,38 @@
+package com.shop.cart_service.dto;
+
+import com.shop.cart_service.model.Cart;
+import com.shop.cart_service.model.CartItem;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class CartMapper {
+    public CartDTO toDto(Cart cart) {
+        if (cart == null) return null;
+        return new CartDTO(
+                cart.getId(),
+//                cart.getUser() != null ? cart.getUser().getId() : null,
+                cart.getCreatedAt(),
+                cart.getUpdatedAt(),
+                cart.getCartItems().stream()
+                        .map(this::toCartItemDto)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    public CartItemDTO toCartItemDto(CartItem cartItem) {
+        if (cartItem == null) return null;
+        return new CartItemDTO(
+                cartItem.getId(),
+                cartItem.getCart() != null ? cartItem.getCart().getId() : null,
+//                cartItem.getProduct() != null ? cartItem.getProduct().getId() : null, // Map productId
+                cartItem.getQuantity(),
+//                cartItem.getProduct() != null ? cartItem.getProduct().getPrice() : null, // Map price from Product
+                cartItem.getCreatedBy(),
+                cartItem.getUpdatedBy(),
+                cartItem.getCreatedAt(),
+                cartItem.getUpdatedAt()
+        );
+    }
+}
---
+++ cart-service/src/main/java/com/shop/cart_service/model/Cart.java
@@ -0,0 +1,44 @@
+package com.shop.cart_service.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private long userId;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "userId", nullable = false)
+//    private User user;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<CartItem> cartItems = new ArrayList<>();
+}
